0fcc16714f04d073ee3112943d8b37775cbee2c1
happier <happier.yiyaoliu@gmail.com>
2011-12-05 17:27:08 -0500

    comment in header file

--- Board.h
+++ Board.h
@@ -15,27 +15,50 @@ class Ai;
 class Board {
     private:
         Block * activeBlock; 
+	  //store the pointer to the block which could move around
+
         QuadrisGame * game;
+	  //pointer to the main game
 
         int filledCellsInRow[ numRows ]; 
-        Block * theBoard[ numColumns  ][ numRows ];
+	  //record the number of filled cells on each row
 
+        Block * theBoard[ numColumns  ][ numRows ];
+	  //record the pointer to block on each cell
+
+       	//private helper function
         void removeRow( int );
+	  //remove a filled row, and transfer rows above it 1 row down
+	
 
     public:
         Board( QuadrisGame * );
         ~Board();
 
         Block * getActiveBlock();
+ 	  //get the pointer pointing to the block that can be moved
+
         bool setActiveBlock( Block * );
+	  //set the pointer for the moving block
+	  //if failed, game ends
 
         bool cellOccupied( Block * , std::pair<int,int> );
+	  //check if the cell is occupied
+
         void addBlock( Block * );
+  	  //called from block, add new block to the board
+
         void deleteBlock( Block * );
-        void clearFilledRows();    //check if any row is completely filled
+	  //called from block, delete the whole block
+
+        void clearFilledRows();    
+	  //check if any row is completely filled
 
         void print();
+	  //print the board by text
+
         void draw( Xwindow * );
+	  //draw the board on Xwindow
  
         friend class Ai;
 };
--- Level.h
+++ Level.h
@@ -1,4 +1,4 @@
-#ifndef __LEVEL_H__
+#ifndef __LEVEL_H__aa
 #define __LEVEL_H__
 
 #include <fstream>
@@ -14,6 +14,7 @@ class Xwindow;
  * Encapsulates level calculations, including determining the type of the next
  * block and creating said block to pass to the Board.
  */
+
 class Level {
     private:
         Board * board;
@@ -21,22 +22,33 @@ class Level {
 
         int currentLevel;
         blockType nextBlockType;
-        std::ifstream blockSequenceFile; // Will read from "sequence.txt"
-        PRNG randomNumGenerator;
 
-        blockType numToBlockType( int ); // Like an immutable map
+        std::ifstream blockSequenceFile; 
+	// Will read from "sequence.txt"
+
+        PRNG randomNumGenerator;
+	//random number generator for level 1~3
+
+	//private helper function
+        blockType numToBlockType( int ); 
+	  // Like an immutable map
+
         void restart();
+	  //clear current 
+
         int generateBlockNumber();
+	  //random number generator 
+
         void findNextBlockType();
 
     public:
         Level( Board * , QuadrisGame * , int seed = -1 );
-        Block * getNextBlock();
-        int getLevel();
-        void levelUp();
-        void levelDown();
-        void printNext();
-        void drawNext(Xwindow *);
+        Block * getNextBlock(); //get the pointer to next block
+        int getLevel();  //get current level
+        void levelUp();  //increase level by 1
+        void levelDown();  //decrease level by 1
+        void printNext();  // print next block in text
+        void drawNext(Xwindow *); // draw next block in Xwindow 
 };
 
 #endif
